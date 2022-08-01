99063b5ba2e25d9b6edc2f8013db24a982d6cfa7
ryanh0428 <99303294+ryanh0428@users.noreply.github.com>
2022-08-01 17:59:39 +0100

    push and replace method in history prop

--- src/components/productDetails.jsx
+++ src/components/productDetails.jsx
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 
 class ProductDetails extends Component {
   handleSave = () => {
-    // Navigate to /products
+    this.props.history.replace("/products"); //when click save , edit the URL and go to that page, add current address to history
+    //if use replace, won't go back to form because current address is replaced(a common behaviour in log in page) 
+    // this.props.history.push("/products");
   };
 
   render() {
