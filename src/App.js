959016737aa9c5c6234c65aabf50ca8bda6baba0
ryanh0428 <99303294+ryanh0428@users.noreply.github.com>
2022-08-01 17:44:24 +0100

    First commit- how to do routing with react

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Products from "./components/products";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import Dashboard from "./components/admin/dashboard";
+import ProductDetails from "./components/productDetails";
+import NotFound from "./components/notFound";
+import "./App.css";
+import { render } from "react-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Route path="/products/:id" component={ProductDetails} />
+            <Route path="/products" component={Products} />{/* while using switch, the order of route should be from most
+          specific one to the most generic one*/ }
+            {/* <Route path="/posts" component={Posts} /> */}
+            <Route path="/posts/:year?/:month?" render={(props) => <Posts sortBy="newest" {...props} />} />
+            <Route path="/admin" component={Dashboard} />
+            < Redirect from="/messages" to="posts" />
+            <Route path="/not-found" component={NotFound} />
+            <Route path="/" exact component={Home} />
+            <Redirect to="/not-found" />
+          </Switch>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
